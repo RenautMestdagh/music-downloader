776d92e1e9f564e4dc136154fa24cb12a3672ee5
RenautMestdagh <renaut.mestdagh@hotmail.com>
2025-09-24 00:27:20 +0200

    url fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(basePath, express.static(path.join(__dirname, 'public')));
 app.use(
     session({
         resave: false,
